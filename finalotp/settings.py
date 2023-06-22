c354489b388ae9cd1148b5c2b4ac420e15642f6d
radhikagujral <gujral.radhika11@gmail.com>
2023-06-22 13:50:17 +0530

    second commit

--- finalotp/settings.py
+++ finalotp/settings.py
@@ -142,3 +142,7 @@ EMAIL_HOST_USER = 'gujral.radhika11@gmail.com'
 
 LOGIN_URL='membership/'
 
+import os
+STATIC_URL ='static/'
+STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
